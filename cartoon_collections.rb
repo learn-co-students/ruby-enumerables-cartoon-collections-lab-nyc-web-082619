4f0f0491a29e23be2572701c03d1ab8baf4c3504
Evan Krone <evankrone1@gmail.com>
2019-08-21 23:38:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+ dwarves = [ "Dopey", "Bashful", "Grumpy", "Doc", "Happy", "Sleepy", "Sneezy"]
+   dwarves.each_with_index{ |element, index|
+   puts "#{index} .#{element}\n"
+     }
+   end
+
+def summon_captain_planet(array)
+  array = array.map{ |element| element.capitalize + "!"
+  }
+  end
+
+def long_planeteer_calls(array)
+short_words = array.select { |element| element.length < 4} 
+  if short_words.count >= 1
+    p false
+  end
+long_words = array.select { |element| element.length > 4} 
+  if long_words.count >=1
+   p true
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+  
+  
 
 def find_the_cheese# code an argument here
   # the array below is here to help
